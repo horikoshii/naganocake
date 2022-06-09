18df70d35f728b48becc74cd4d56915258aae8bf
EC2 Default User <ec2-user@ip-172-31-1-185.ec2.internal>
2022-06-09 11:02:22 +0000

    admin-12

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
+  def full_name
+    this.last_name + this.first_name
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
